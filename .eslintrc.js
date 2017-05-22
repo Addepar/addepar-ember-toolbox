51250c786dbdf85ca67cfe322bfc6c23897747b0
Christopher Garrett <me@pzuraq.com>
2017-05-22 10:23:27 -0700

    Initial setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,16 @@ module.exports = {
     ecmaVersion: 2017,
     sourceType: 'module'
   },
-  extends: 'eslint:recommended',
+  extends: [
+    'eslint:recommended',
+    'plugin:ember-suave/recommended'
+  ],
   env: {
     browser: true
   },
   rules: {
+    'quotes': ['error', 'single', {  "allowTemplateLiterals": true, "avoidEscape": true }],
+    'ember-suave/no-const-outside-module-scope': 0,
+    'ember-suave/no-direct-property-access': 1
   }
 };
