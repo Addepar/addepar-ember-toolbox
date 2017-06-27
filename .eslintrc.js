9a54d9030f18738003c1111e9d73c2e6db12a6e6
Christopher Garrett <me@pzuraq.com>
2017-06-26 20:12:02 -0700

    feature(property): Adds property decorator and docs

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,8 +12,11 @@ module.exports = {
     browser: true
   },
   rules: {
-    'quotes': ['error', 'single', {  "allowTemplateLiterals": true, "avoidEscape": true }],
+    'camelcase': ['error', { 'properties': 'never' }],
+    'quotes': ['error', 'single', {  'allowTemplateLiterals': true, 'avoidEscape': true }],
     'ember-suave/no-const-outside-module-scope': 0,
-    'ember-suave/no-direct-property-access': 1
+    'ember-suave/no-direct-property-access': 1,
+    'ember-suave/require-access-in-comments': 0,
+    'prefer-const': 2
   }
 };
--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 'use strict';
 
 module.exports = {
-  name: 'ice-box'
+  name: '@addepar/ice-box'
 };
