48ee69afbe5ba43aab5e594d952f010c26cfa610
Alex Navasardyan <alex.navasardyan@gmail.com>
2017-12-08 13:44:19 -0800

    Refrain from using arrow functions in vendor file
    When using `app.import` API and including dependencies into the host
    app, imported files don't get transpiled. That would lead to issues in
    browsers which don't support latest JS syntax (IE 11).
    
    Relevant links:
    
    + https://caniuse.com/#feat=arrow-functions

--- vendor/ice-box/register-legacy-helpers.js
+++ vendor/ice-box/register-legacy-helpers.js
@@ -1,7 +1,7 @@
-Ember.HTMLBars._registerHelper('concat', Ember.HTMLBars.makeBoundHelper((params) => {
+Ember.HTMLBars._registerHelper('concat', Ember.HTMLBars.makeBoundHelper(function(params) {
   return params.join('');
 }));
 
-Ember.HTMLBars._registerHelper('hash', Ember.HTMLBars.makeBoundHelper((params, hash) => {
+Ember.HTMLBars._registerHelper('hash', Ember.HTMLBars.makeBoundHelper(function(params, hash) {
   return hash;
 }));
