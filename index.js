777dcff84045c15a3cf3b89bce45795e10247d96
Christopher Garrett <me@pzuraq.com>
2018-04-05 16:43:20 -0700

    [refactor] Update package name and remove styles

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 const VersionChecker = require('ember-cli-version-checker');
 
 module.exports = {
-  name: '@addepar/ice-box',
+  name: '@addepar/ember-toolbox',
 
   included(app) {
     const emberChecker = new VersionChecker(app).forEmber();
@@ -12,29 +12,5 @@ module.exports = {
     if (emberChecker.lt('1.13.0')) {
       app.import('vendor/ice-box/register-legacy-helpers.js');
     }
-
-    app.options = app.options || {};
-    app.options.sassOptions = app.options.sassOptions || {};
-
-    const sass = require('node-sass');
-    const importOnce = require('node-sass-import-once');
-
-    if (!app.options.sassOptions.nodeSass) {
-
-      const sassRender = sass.render;
-
-      sass.render = function(options, callBack) {
-        options.importer = importOnce;
-        options.importOnce = {
-          index: false,
-          css: false,
-          bower: false
-        };
-
-        sassRender(options, callBack);
-      }
-    }
-
-    app.options.sassOptions.nodeSass = sass;
   }
 };
