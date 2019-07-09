f40821ba330ed884fa670f104aadc7c027be9104
Matthew Beale <matt.beale@addepar.com>
2019-07-09 08:46:57 -0400

    Have the linter always use the application's eslint

--- .lint-staged.config.js
+++ .lint-staged.config.js
@@ -1,5 +1,7 @@
+const path = require('path');
 const prettierPath = require.resolve('prettier/bin-prettier');
-const eslintPath = require.resolve('eslint/bin/eslint');
+const parentNodePath = path.resolve(path.join(path.basename(__filename), '../node_modules/'));
+const eslintPath = require.resolve('eslint/bin/eslint', { paths: [parentNodePath] });
 const sassLintPath = require.resolve('sass-lint/bin/sass-lint');
 
 module.exports = {
