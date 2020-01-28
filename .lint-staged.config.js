2074e523e46ae13058d35b0055a4f95c2c37b893
Cory Forsyth <cory.forsyth@addepar.com>
2020-01-28 11:04:43 -0500

    Upgrade lint-staged from 7.3.0 to ^10.0.3

--- .lint-staged.config.js
+++ .lint-staged.config.js
@@ -1,14 +1,15 @@
+/* eslint-env node */
+
 const path = require('path');
+
 const prettierPath = require.resolve('prettier/bin-prettier');
 const parentNodePath = path.resolve(path.join(path.basename(__filename), '../node_modules/'));
 const eslintPath = require.resolve('eslint/bin/eslint', { paths: [parentNodePath] });
 const sassLintPath = require.resolve('sass-lint/bin/sass-lint');
 
 module.exports = {
-  linters: {
-    '*.{js,json,md,scss}': [`${prettierPath} --write`, 'git add'],
-    '*.js': [`${eslintPath} --fix`, 'git add'],
-    '*.scss': `${sassLintPath} -vq`,
-    '*': 'ember adde-lint --file-names',
-  },
+  '*.{js,json,md,scss}': [`${prettierPath} --write`],
+  '*.js': [`${eslintPath} --fix`],
+  '*.scss': `${sassLintPath} -vq`,
+  '*': 'ember adde-lint --file-names',
 };
